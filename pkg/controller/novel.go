9abe207de84fc7def5936fd063f49ea49bdd1690
Vitor Savian <vitor.savian@suse.com>
2025-03-23 18:56:05 -0300

    chore: create novel controller

--- pkg/controller/novel.go
+++ pkg/controller/novel.go
@@ -1,1 +1,30 @@
 package novel
+
+import (
+	"fmt"
+	"sync"
+)
+
+type NovelController struct {
+}
+
+var novelLock = &sync.Mutex{}
+
+var NovelControllerInstance *NovelController
+
+func getNovelControllerInstance() *NovelController {
+	if NovelControllerInstance == nil {
+		novelLock.Lock()
+		defer novelLock.Unlock()
+
+		if NovelControllerInstance == nil {
+			NovelControllerInstance = &NovelController{}
+		} else {
+			fmt.Println("Novel controller instance already created")
+		}
+	} else {
+		fmt.Println("Novel controler instance already created")
+	}
+
+	return NovelControllerInstance
+}
