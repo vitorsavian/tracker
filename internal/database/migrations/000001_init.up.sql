2a805bdf24a342bbd143052b9e18ac5b62024f1a
Vitor Savian <vitor.savian@suse.com>
2025-03-26 15:39:28 -0300

    feat: add novel sql

--- internal/database/migrations/000001_init.up.sql
+++ internal/database/migrations/000001_init.up.sql
@@ -1,8 +1,8 @@
-DO $$
-BEGIN
-
-
-END
-$$;
-
-CREATE  IF NOT EXISTS
+CREATE TABLE novel (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    name VARCHAR(512) NOT NULL,
+    page SMALLINT,
+    finished BOOLEAN DEFAULT FALSE
+);
