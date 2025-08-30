699c0e091be9c49153392a226bfdcba046a28767
vitor savian <vitor.savian@suse.com>
2025-08-30 03:09:43 -0300

    feat: add more stuff to cors

--- pkg/controller/rest/server.go
+++ pkg/controller/rest/server.go
@@ -55,8 +55,14 @@ func GetControllerInstance() *Controller {
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Max-Age", "600")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
