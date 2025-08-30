6a7ee8743d74ad99811c03c9abb1b9cd0d6a51d9
vitor savian <vitor.savian@suse.com>
2025-08-30 02:23:13 -0300

    fix: does not break cors anymore

--- pkg/controller/rest/server.go
+++ pkg/controller/rest/server.go
@@ -54,9 +54,9 @@ func GetControllerInstance() *Controller {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Acess-Control-Allow-Origin", "*")
-		w.Header().Set("Acess-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		w.Header().Set("Acess-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		next.ServeHTTP(w, r)
 	})
